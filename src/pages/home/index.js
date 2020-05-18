aba019daa66392961ea06ac56a13a575d311e46f
Luiz Felicio <unifelicio@gmail.com>
2020-05-18 12:37:59 -0300

    refactor: home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,22 +1,15 @@
 import React, { Suspense, memo } from 'react';
 
 import Layout from 'containers/Layout';
-import TicketList from 'components/TicketList';
-
-import { useTicketList } from 'context/ticketList';
-import { getTickets } from 'services/api';
+import TicketList from 'containers/TicketList';
 
 const HomePage = () => {
-  const [state, dispatch] = useTicketList();
-
   React.useEffect(() => console.log('HomePage rendered'));
-
-  // React.useEffect(() => dispatch({ type: 'UPDATE_TICKET_LIST', payload: getTickets() }), []);
 
   return (
     <Layout>
       <Suspense fallback={<h1>Loading suspense...</h1>}>
-        <TicketList resource={state.ticketList} />
+        <TicketList />
       </Suspense>
     </Layout>
   );
